393e9aaed62b706b3e0b4d86bbb12a21a6758b17
CSeongBeom <49608119+CSeongBeom@users.noreply.github.com>
2025-08-06 00:01:29 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int num;
+	scanf("%d", &num);
+
+
+	for (int i = 0; i < num; i++)
+	{
+		for (int j = 0; j < num-i-1; j++)
+		{
+			printf(" ");
+		}
+		for (int k = 0; k < (2 * i + 1); k++)
+		{
+			printf("*");
+		}
+	printf("\n");
+	}
+	for (int i = 1; i < num; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			printf(" ");
+		}
+		for (int k = 0; k < (2*num - 2*i - 1); k++)
+		{
+			printf("*");
+		
+		}
+	printf("\n");
+	}
+
+
+	return 0;
+}
