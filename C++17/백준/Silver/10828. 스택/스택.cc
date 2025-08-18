ac1370149a396ce7d74b5498b7ccb751cd884a86
CSeongBeom <49608119+CSeongBeom@users.noreply.github.com>
2025-08-18 23:15:29 +0900

    [Silver IV] Title: 스택, Time: 216 ms, Memory: 2156 KB -BaekjoonHub

---
+++ C++17/백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int N,n;
+	cin >> N;
+	vector<int> stack;
+	string command;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> command;
+		if (command == "push")
+		{
+			cin >> n;
+			stack.push_back(n);
+		}
+		else if (command == "top")
+		{
+			if (stack.empty())
+				cout << "-1" << endl;
+			else
+				cout << stack.back() << endl;
+		}
+		else if (command == "size")
+		{
+			cout << stack.size() << endl;
+		}
+		else if (command == "pop")
+		{
+			if (stack.empty())
+				cout << "-1" << endl;
+			else
+			{
+				cout << stack.back() << endl;
+				stack.pop_back();
+			}
+		}
+		else if (command == "empty")
+		{
+			cout << stack.empty() << endl;
+		}
+	}
+
+}
